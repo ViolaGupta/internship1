e71fe5e892910388e1541a1e9fcf26ade15cc785
ViolaGupta <viola.gupta@yahoo.in>
2016-07-25 13:33:23 +0530

    searchresult

---
+++ searchresult.php
@@ -0,0 +1,378 @@
+
+<html >
+	<head>
+        	<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Best Restaurants In India</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- CSS
+        ================================================ -->
+        <!-- Owl Carousel -->
+		<link rel="stylesheet" href="css/owl.carousel.css">
+        <!-- bootstrap.min css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Font-awesome.min css -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+        <!-- Main Stylesheet -->
+        <link rel="stylesheet" href="css/animate.min.css">
+
+		<link rel="stylesheet" href="css/main.css">
+        <!-- Responsive Stylesheet -->
+		<link rel="stylesheet" href="css/responsive.css">
+		<!-- Js -->
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> -->
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+    <script src="js/jquery.nav.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/main.js"></script>
+        <style>
+  
+    .outsidebox{
+        position: relative;
+        
+        min-height: 110px;
+        
+        font-family: ;
+        margin-top: 260px;
+        
+        
+    }
+            .bg{
+                border-radius: 5px;
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                background-color: black;
+                opacity: 0.5;
+                 
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+            }
+    .innerwrapper{
+                font-family: myfont;
+
+       
+        padding-bottom: 10px;
+     
+    }
+    select {
+        
+        padding:3px;
+        height: 60px;
+        float: left;
+        background: url(http://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/br_down.png) no-repeat right ;
+        
+        -webkit-appearance: none;
+        border-radius: 5px 0px 0px 5px;
+        
+        font-size: 16px;
+        border: 0;
+        background-color: white;
+      /* background: url(http://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/br_down.png) no-repeat right #ddd; */
+        background-color: white;
+        -webkit-appearance: none;
+  
+}
+    .locality{
+        float: left;
+        padding-left: 10px;
+       
+        margin: 0;
+        
+        height: 60px;
+        font-size: 16px;
+        border: 0;
+        background-color: lightgrey; 
+    }
+    .btn{
+        float: left;
+       border: 0;
+    padding: 10px;
+        height: 60px;
+    font-size: 16px;
+  
+    border-radius: 0px 5px 5px 0px;
+   
+   
+        background-color: darkcyan; 
+        color: black;
+    }
+            
+   
+           
+            #overlay    {  
+                background:rgba(0,0,0,.75);
+                text-align:center;
+                height:150px;
+                padding:65px 0 66px 0;
+                opacity:0;
+                -webkit-transition: opacity .25s ease;
+                -moz-transition: opacity .25s ease;
+            }
+
+            #box1:hover #overlay {
+                 opacity:1;
+            }
+            #box1    {  
+                 background:url(images/slider/slider-img-1.jpg);
+                 background-size:cover;
+                 overflow:hidden;
+                 margin-right: 10px;
+            height: 150px;}
+            #box2:hover #overlay {
+                 opacity:1;}
+            #box2    {  
+                 background:url(images/slider/slider-img-2.jpg);
+                 background-size:cover;
+                 overflow:hidden;
+                 margin-right: 10px;
+            height: 150px;}
+            #box3:hover #overlay {
+                 opacity:1;}
+            #box3    {  
+                 background:url(images/slider/slider-img-3.jpg);
+                 background-size:cover;
+                 overflow:hidden;
+                 margin-right: 10px;
+            height: 150px;}
+            #box4:hover #overlay {
+                 opacity:1;}
+            #box4    {  
+                 background:url(images/slider/slider-img-4.jpg);
+                 background-size:cover;
+                 overflow:hidden;
+                 margin-right: 10px;
+            height: 150px;}
+            #box5:hover #overlay {
+                 opacity:1;}
+            #box5    {  
+                 background:url(images/slider/slider-img-1.jpg);
+                 background-size:cover;
+                 overflow:hidden;
+                 margin-right: 10px;
+            height: 150px;}
+            #box6:hover #overlay {
+                 opacity:1;}
+            #box6    {  
+                 background:url(images/slider/slider-img-2.jpg);
+                 background-size:cover;
+                 overflow:hidden;
+                 margin-right: 10px;
+            height: 150px;}
+            #box7:hover #overlay {
+                 opacity:1;}
+            #box7    {  
+                 background:url(images/slider/slider-img-3.jpg);
+                 background-size:cover;
+                 overflow:hidden;
+                 margin-right: 10px;
+                 height: 150px;
+            }
+            slidertext{
+                color: white;
+            }
+            #box8:hover #overlay {
+                 opacity:1;}
+            #box8    {  
+                 background:url(images/slider/slider-img-4.jpg);
+                 background-size:cover;
+                 overflow:hidden;
+                 margin-right: 10px;
+            height: 150px;}
+            slidertext{
+                padding-left: 5px;
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+        <section id="hero-area">
+             
+        <div class="outsidebox col-xs-10 col-xs-offset-1">
+            <div class="bg"></div>
+    <div class="innerwrapper col-xs-12">
+        <h4 style="color:white; text-align:center; font-family:Arial, Helvetica, sans-serif ;padding-top:10px; padding-bottom:10px; font-size:25px">Best Restaurants in Town</h4>
+    
+       <div class="row">
+           <form  method="POST" >
+                 <select  class="col-xs-12 col-sm-3 conatiner-fluid" id="city" name="city">
+                     <option value="">Choose City</option><span class="caret"></span> 
+                     <option value="Bangalore">Bangalore</option>
+                     <option value="New Delhi">New delhi</option>
+                     <option value="Mumbai">Mumbai</option>
+                     <option value="Pune">Pune</option>
+                     <option value="Chennai">Chennai</option>
+                     <option value="Hyderabad">Hyderabad</option>
+                     <option value="Hyderabad">Kolkata</option>
+                     <option value="Hyderabad">Visakhapatnam</option>
+                 </select>
+
+              <input class="col-xs-12 col-sm-6  locality" type="search" placeholder="Type Your location (landmark,road,area)" autocomplete="off" name="locality">
+                  
+               
+                  <input type="submit" name="submit" value="Search" class="col-sm-3 col-xs-12 btn">
+               </form>
+                </div>
+    
+        </div>
+    </div>
+      
+       
+   </section>
+        <?php     
+
+        function lookup($string){
+ 
+   $string = str_replace (" ", "+", urlencode($string));
+   $details_url = "http://maps.googleapis.com/maps/api/geocode/json?address=".$string."&sensor=false";
+ 
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, $details_url);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   $response = json_decode(curl_exec($ch), true);
+ 
+   // If Status Code is ZERO_RESULTS, OVER_QUERY_LIMIT, REQUEST_DENIED or INVALID_REQUEST
+   if ($response['status'] != 'OK') {
+    return null;
+   }
+ 
+   
+            echo "<br>";
+   $geometry = $response['results'][0]['geometry'];
+ 
+    $longitude = $geometry['location']['lng'];
+    $latitude = $geometry['location']['lat'];
+ 
+    $array = array(
+        'latitude' => $geometry['location']['lat'],
+        'longitude' => $geometry['location']['lng'],
+        'location_type' => $geometry['location_type'],
+    );
+ 
+    return $array;
+ 
+}
+ 
+$city = $_POST['locality'].','.$_POST['city'];
+ 
+$array = lookup($city);
+// print $array['latitude']."<br>".$array['longitude']."<br>";
+ 
+// echo $city."<br>";
+// Get cURL resource
+
+         $curl = curl_init();
+
+// Curl options
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => ['Accept: application/json', 'user-key: 3b659dd898c0058acd1d609a02a02d3c'],
+    CURLOPT_URL => 'https://developers.zomato.com/api/v2.1/search?entity_type=subzone&count=6&lat='.$array['latitude'].'&lon='.$array['longitude'].'&radius=2000&category=2&sort=rating&order=desc',
+));
+
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+
+// Check for errors if curl_exec fails
+if(!curl_exec($curl)){
+    die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
+}
+
+// Close request to clear up some resources
+curl_close($curl);
+
+// dumping $resp
+// echo 'var_dump: <br>';
+// var_dump($resp);
+echo '<br><br>';
+
+// Decode json
+$jsonZomato = json_decode($resp, true);
+
+// print readable results
+// print "<pre>";
+  //     print_r($jsonZomato);
+ for($t=0; $t<10; $t++)
+{// print $jsonZomato['restaurants'][$t]['restaurant']['name']."<br>";
+        }
+print "</pre>";
+
+            ?>
+  
+        <section id="blog">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="block">
+                        <h1 class="heading">Best <span>Restaurants</span> in your <span>locality</span></h1>
+                        <ul>
+                            <li class="wow fadeInLeft" data-wow-duration="300ms" data-wow-delay="300ms">
+                                <div class="blog-img">
+                                     <img src="images/blog/blog-img-1.jpg" alt="blog-img">
+                                </div>
+                                <div class="content-right">
+                                    <h3><?php echo $jsonZomato['restaurants'][0]['restaurant']['name'] ?></h3>
+                                    <p><?php echo $jsonZomato['restaurants'][0]['restaurant']['location']['address']."<br>".$jsonZomato['restaurants'][0]['restaurant']['cuisines']  ?> </p>
+                                </div>
+                            </li>
+                            <li class="wow fadeInLeft" data-wow-duration="300ms" data-wow-delay="400ms">
+                                <div class="blog-img">
+                                    <img src="images/blog/blog-img-2.jpg" alt="blog-img">
+                                </div>
+                                <div class="content-right">
+                                    <h3><?php echo $jsonZomato['restaurants'][1]['restaurant']['name'] ?></h3>
+                                    <p><?php echo $jsonZomato['restaurants'][1]['restaurant']['location']['address']."<br>".$jsonZomato['restaurants'][1]['restaurant']['cuisines']  ?> </p>
+                                </div>
+                            </li>
+                            <li class="wow fadeInLeft" data-wow-duration="300ms" data-wow-delay="500ms">
+                                <div class="content-left">
+                                    <h3><?php echo $jsonZomato['restaurants'][2]['restaurant']['name'] ?></h3>
+                                   <p><?php echo $jsonZomato['restaurants'][2]['restaurant']['location']['address']."<br>".$jsonZomato['restaurants'][2]['restaurant']['cuisines']  ?> </p>
+                                </div>
+                                <div class="blog-img-2">
+                                    <img src="images/blog/blog-img-3.jpg" alt="blog-img">
+                                </div>
+                            </li>
+                            <li class="wow fadeInLeft" data-wow-duration="300ms" data-wow-delay="600ms">
+                                <div class="content-left">
+                                    <h3><?php echo $jsonZomato['restaurants'][3]['restaurant']['name'] ?></h3>
+                                   <p><?php echo $jsonZomato['restaurants'][3]['restaurant']['location']['address']."<br>".$jsonZomato['restaurants'][3]['restaurant']['cuisines']  ?> </p>
+                                </div>
+                                <div class="blog-img-2">
+                                    <img src="images/blog/blog-img-4.jpg" alt="blog-img">
+                                </div>
+                            </li>
+                            <li class="wow fadeInLeft" data-wow-duration="300ms" data-wow-delay="700ms">
+                                <div class="blog-img">
+                                    <img src="images/blog/blog-img-5.jpg" alt="blog-img">
+                                </div>
+                                <div class="content-right">
+                                    <h3><?php echo $jsonZomato['restaurants'][4]['restaurant']['name'] ?></h3>
+                                   <p><?php echo $jsonZomato['restaurants'][4]['restaurant']['location']['address']."<br>".$jsonZomato['restaurants'][4]['restaurant']['cuisines']  ?> </p>
+                                </div>
+                            </li>
+                            <li class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="800ms">
+                                <div class="blog-img">
+                                    <img src="images/blog/blog-img-6.jpg" alt="blog-img">
+                                </div>
+                                <div class="content-right">
+                                    <h3><?php echo $jsonZomato['restaurants'][5]['restaurant']['name'] ?></h3>
+                                    <p><?php echo $jsonZomato['restaurants'][5]['restaurant']['location']['address']."<br>".$jsonZomato['restaurants'][5]['restaurant']['cuisines']  ?> </p>
+                                </div>
+                            </li>
+                        </ul>
+                        <a class="btn btn-default btn-more-info wow bounceIn" data-wow-duration="500ms" data-wow-delay="1200ms" href="#" role="button">More Info</a>
+                    </div>
+                </div><!-- .col-md-12 close -->
+            </div><!-- .row close -->
+        </div><!-- .containe close -->
+    </section><!-- #blog close -->
+    </body>
+</html>
